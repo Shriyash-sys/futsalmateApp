ec88825ba2b7c94510bddf2c1e18b56daf745224
shriyash-w3 <shriyash.amatya@w3developers.com.np>
2025-11-19 12:11:08 +0545

    Add initial project structure with routes, tests, and configuration files
    - Created console command for inspiring quotes in routes/console.php
    - Defined a welcome route in routes/web.php
    - Added .gitignore files for storage directories to exclude unnecessary files
    - Implemented example tests in tests/Feature and tests/Unit
    - Set up Pest testing framework in tests/Pest.php
    - Configured Vite for asset management in vite.config.js

---
+++ app/Http/Controllers/SignupControllerAPI.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class SignupControllerAPI extends Controller
+{
+    public function signup(Request $request)
+    {
+        $validatedData = $request->validate([
+            'full_name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users, email',
+            'password' => 'required|string|min:8|confirmed',
+            'phone' => 'nullable|string|max:15|unique:users, phone',
+            'terms' =>'accepted'
+        ]);
+
+        $user = User::create([
+            'full_name' => $validatedData['full_name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'phone' => $validatedData['phone'] ?? null,
+        ]);
+
+        Auth::login($user);
+
+        try {
+            $user->sendEmailVerificationNotification();
+        } catch (Exception $e) {
+            Log::error('Verification email failed to send: ' .$e->getMessage());
+            return response()->json([
+                'status' => 'warning',
+                'message' => 'Registered, but verification email failed. Please try again later.'
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Registration successful! Please check your email to verify your account.',
+            'user' => $user
+        ], 201);
+    }
+}
