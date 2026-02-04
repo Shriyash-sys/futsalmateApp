244509337c8b88ef7430f436ceb092f4d352696e
shriyash-sys <amatyashriyash16@gmail.com>
2026-02-04 14:40:53 +0545

    feat: Update middleware for court booking and user profile routes to require customer role

--- routes/api.php
+++ routes/api.php
@@ -33,12 +33,12 @@ Route::post('/email/verify/resend-otp', [EmailVerificationController::class, 're
 Route::get('/user-dashboard', [UserProfileControllerAPI::class, 'userDashboard']);
 
 //Show available courts for booking
-Route::middleware('auth:sanctum')->get('/show-court', [CourtControllerAPI::class, 'showBookCourt']);
-Route::middleware('auth:sanctum')->get('/court-detail/{courtId}', [CourtControllerAPI::class, 'showCourtDetail']);
+Route::middleware(['auth:sanctum', 'customer'])->get('/show-court', [CourtControllerAPI::class, 'showBookCourt']);
+Route::middleware(['auth:sanctum', 'customer'])->get('/court-detail/{courtId}', [CourtControllerAPI::class, 'showCourtDetail']);
 
 
 // Booking endpoints
-Route::middleware('auth:sanctum')->controller(BookControllerAPI::class)->group(function () {
+Route::middleware(['auth:sanctum', 'customer'])->controller(BookControllerAPI::class)->group(function () {
     Route::post('/book', 'bookCourt');
     Route::get('/book/esewa/success', 'success');
     Route::get('/book/esewa/failure', 'failure');
@@ -50,7 +50,7 @@ Route::middleware('auth:sanctum')->controller(BookControllerAPI::class)->group(f
 });
 
 // Community (team) registration
-Route::middleware('auth:sanctum')->controller(CommunityControllerAPI::class)->group(function () {
+Route::middleware(['auth:sanctum', 'customer'])->controller(CommunityControllerAPI::class)->group(function () {
     Route::post('/community/register-team', 'registerTeam');    
     Route::get('/community/user-communities', 'showTeams');
     Route::delete('/community/delete-team/{id}', 'deleteTeam');
@@ -58,7 +58,7 @@ Route::middleware('auth:sanctum')->controller(CommunityControllerAPI::class)->gr
 });
 
 // User profile
-Route::middleware('auth:sanctum')->controller(UserProfileControllerAPI::class)->group(function () {
+Route::middleware(['auth:sanctum', 'customer'])->controller(UserProfileControllerAPI::class)->group(function () {
     Route::get('/profile', 'show');
     Route::match(['put','patch'],'/profile', 'editProfile');
     Route::post('/profile/photo', 'addProfilePhoto');
@@ -66,7 +66,7 @@ Route::middleware('auth:sanctum')->controller(UserProfileControllerAPI::class)->
 });
 
 // Logout endpoints (require authentication)
-Route::middleware('auth:sanctum')->post('/logout', [LoginControllerAPI::class, 'logout']);
+Route::middleware(['auth:sanctum', 'customer'])->post('/logout', [LoginControllerAPI::class, 'logout']);
 
 
 
